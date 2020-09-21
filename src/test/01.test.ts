e977284d56749f227fc388437320eb1ef15985c4
pastukhVitalii <pastukh.v.7@gmail.com>
2020-09-21 23:09:04 +0300

    tests function 'splitIntoWords'

--- src/test/01.test.ts
+++ src/test/01.test.ts
@@ -1,27 +1,33 @@
-import {mult, sum} from "./01";
+import {mult, splitIntoWords, sum} from "./01";
+// data
+
+let a: number;
+let b: number;
+let c: number;
+
+// callback 'beforeEach' starts before each tests
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
 
 test('sum should be correct', () => {
-    // data
-    const a = 1;
-    const b = 2;
-    const c = 3;
-
     // action
     const result1 = sum(a, b);
+
+    // change 'a' only this test
+    a = 100
     const result2 = sum(a, c);
 
     // expect result
     expect(result1).toBe(3)
-    expect(result2).toBe(4)
+    expect(result2).toBe(103)
 })
 
 test('multiply should be correct', () => {
-    // data
-    const a = 1;
-    const b = 2;
-    const c = 3;
-
     // action
+    // a = 1 not 100
     const result1 = mult(a, b);
     const result2 = mult(b, c);
 
@@ -29,4 +35,24 @@ test('multiply should be correct', () => {
     expect(result1).toBe(2)
     expect(result2).toBe(6)
 
+})
+
+test('splitting into words should be correct', () => {
+    // data
+    const sent1 = 'Hello my friend!';
+    const sent2 = 'JS - the best programming language';
+
+    // action
+    const result1 = splitIntoWords(sent1);
+    const result2 = splitIntoWords(sent2);
+
+    // expect result
+
+    expect(result1.length).toBe(3);
+    expect(result1[0]).toBe('hello');
+    expect(result1[1]).toBe('my');
+
+    expect(result2.length).toBe(5);
+    expect(result2[0]).toBe('js');
+    expect(result2[1]).toBe('the');
 })
--- src/test/01.ts
+++ src/test/01.ts
@@ -5,3 +5,11 @@ export let mult = (a: number, b: number) => {
     return a * b
 }
 
+export let splitIntoWords = (sentence: string) => {
+    const words = sentence.toLowerCase().split(' ')
+    return words.filter( w => w !== '' && w !== '-')
+        .map(w => w
+            .replace('!', '')
+            .replace('.', '')
+            .replace(',', ''))
+ }
